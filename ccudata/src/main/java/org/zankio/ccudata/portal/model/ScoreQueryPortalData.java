f928c194c19a3b5899aff21927c1fefef884468c
Azzurra Cox <rotiong97@gmail.com>
2019-10-14 16:54:16 +0800

    Fix ScoreQuery

--- ccudata/src/main/java/org/zankio/ccudata/portal/model/ScoreQueryPortalData.java
+++ ccudata/src/main/java/org/zankio/ccudata/portal/model/ScoreQueryPortalData.java
@@ -3,7 +3,8 @@ package org.zankio.ccudata.portal.model;
 public class ScoreQueryPortalData extends PortalData {
     public ScoreQueryPortalData() {
         super(
-                "https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/library/SSO/Query_grade/getssoCcuRight.php",
+                //"https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/library/SSO/Query_grade/getssoCcuRight.php",
+                "https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/Query/Query_grade.php",
                 "0007"
         );
     }
