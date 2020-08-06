1fe4009584e32f446319efb31b1e6f9c2630d806
Azzurra Cox <rotiong97@gmail.com>
2020-08-07 03:38:02 +0800

    map utility

--- cculife/src/main/java/org/zankio/cculife/ui/map/MapActivity.java
+++ cculife/src/main/java/org/zankio/cculife/ui/map/MapActivity.java
@@ -1,11 +1,18 @@
 package org.zankio.cculife.ui.map;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.github.chrisbanes.photoview.PhotoView;
 
 import org.zankio.cculife.R;
 import org.zankio.cculife.ui.base.BaseActivity;
 
 public class MapActivity extends BaseActivity {
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,17 @@ public class MapActivity extends BaseActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mImageView = findViewById(R.id.imageView2);
+
+        mImageView.setOnClickListener(view -> {
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(MapActivity.this);
+            View mView = getLayoutInflater().inflate(R.layout.dialog_custom_layout, null);
+            PhotoView photoView = mView.findViewById(R.id.imageView3);
+            photoView.setImageResource(R.drawable.ccu_map);
+            mBuilder.setView(mView);
+            AlertDialog mDialog = mBuilder.create();
+            mDialog.show();
+        });
 
     }
 }
